cd48dbd890c96eb0cc5de554c53363ca82631e43
Elizabeth Fortanely <efortanely@elizabeths-mbp.lan>
2023-03-14 18:59:28 -0500

    change text on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = ({ open, width, linkColor, hoverColor }) => {
     <StyledAbout open={open} width={width} linkColor={linkColor} hoverColor={hoverColor}>
       <div className='content'>
         <h1>ELIZABETH FORTANELY — SOFTWARE ENGINEER — AUSTIN, TX</h1>
-        <p>I'm a UT Austin alum with a B.S.A in Computer Science with a Studio Art minor. I have 1.5 years of experience in full-stack web development and data visualization. I'm interested in pursuing new roles in full-stack web development or machine learning! In my free time, I've been working on my <a href="https://www.etsy.com/shop/LizziesCozyCrafts">crochet plushie small business</a> and attending local LGBTQ+ events!</p>
+        <p>I'm a UT Austin alum with a B.S.A in Computer Science and a Studio Art minor. I have 1.5 years of experience in full-stack web development and data visualization. I'm interested in pursuing new roles in full-stack web development or machine learning. In my free time, I've been working on my <a href="https://www.etsy.com/shop/LizziesCozyCrafts">crochet plushie small business</a> and attending local LGBTQ+ events!</p>
         <p>If you are interested in working with me or have any questions, please don't hesitate to contact me.</p>
         <Button onClick={(e) => {
           e.preventDefault();
