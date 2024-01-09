f8d6d5cea6f630f0e900bdbcd39cb0a80e7be1f6
Elizabeth Fortanely <elizabeth.fortanely@gmail.com>
2024-01-08 20:03:22 -0600

    try to fix grey bar issue

--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -5,6 +5,7 @@ export const StyledHome = styled.div`
     display: flex;
     flex-direction: column;
     height: 100vh;
+    min-height: 100%;
     max-height: -webkit-fill-available;
     top: 0;
 
